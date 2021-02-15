6225f6947e94d27e4a33c49472257ec1b5e5b77d
Demario Douce <demario.douce567@gmail.com>
2021-02-15 13:00:53 -0500

    Added menu control to implement the selection of home types.

--- app/src/main/java/com/demariodouce/demariodouce_comp304sec003_lab2_ex1/HomeStylesActivity.java
+++ app/src/main/java/com/demariodouce/demariodouce_comp304sec003_lab2_ex1/HomeStylesActivity.java
@@ -3,12 +3,27 @@ package com.demariodouce.demariodouce_comp304sec003_lab2_ex1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class HomeStylesActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_styles);
+        //Palettes
+        Spinner spinner = (Spinner) findViewById(R.id.homeTypeSnr);
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.home_types_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+
     }
 }
