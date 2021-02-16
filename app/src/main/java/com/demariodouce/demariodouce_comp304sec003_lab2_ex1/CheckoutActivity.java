b524e9133ec504db306cf8cc7a323162666223ec
DemarioDouce <demario.douce567@gmail.com>
2021-02-16 15:29:57 -0500

    Changes and improvements.

--- app/src/main/java/com/demariodouce/demariodouce_comp304sec003_lab2_ex1/CheckoutActivity.java
+++ app/src/main/java/com/demariodouce/demariodouce_comp304sec003_lab2_ex1/CheckoutActivity.java
@@ -2,11 +2,16 @@ package com.demariodouce.demariodouce_comp304sec003_lab2_ex1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class CheckoutActivity extends AppCompatActivity {
 
@@ -32,5 +37,29 @@ public class CheckoutActivity extends AppCompatActivity {
             RadioButton radioButton = (RadioButton) findViewById(R.id.radioBtn);
             radioButton.setChecked(radioBoolean);
         }
+        Button  button = (Button) findViewById(R.id.buttonPayment);
+        RadioButton radioButton = (RadioButton) findViewById(R.id.radioBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(radioButton.isChecked() == false){
+
+                    Context context = getApplicationContext();
+                    CharSequence text = "Please select an option.";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+
+                } else {
+                    openPaymentActivity();
+                }
+            }
+        });
+    }
+    public void openPaymentActivity() {
+
+        Intent intent = new Intent(this, PaymentActivity.class);
+        startActivity(intent);
     }
 }
