6b5e2119b66f1185b61abc91eaa87f28aba3012a
DemarioDouce <demario.douce567@gmail.com>
2021-02-16 18:25:18 -0500

    Completed.

--- app/src/main/java/com/demariodouce/demariodouce_comp304sec003_lab2_ex1/ConfirmationActivity.java
+++ app/src/main/java/com/demariodouce/demariodouce_comp304sec003_lab2_ex1/ConfirmationActivity.java
@@ -2,7 +2,18 @@ package com.demariodouce.demariodouce_comp304sec003_lab2_ex1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ConfirmationActivity extends AppCompatActivity {
 
@@ -10,5 +21,47 @@ public class ConfirmationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmation);
+
+        EditText fullName = (EditText) findViewById(R.id.editTextTextPersonName);
+        EditText question1 = (EditText) findViewById(R.id.editTextTextRandomQuestion1);
+        EditText question2 = (EditText) findViewById(R.id.editTextTextRandomQuestion2);
+        EditText paymentMethod = (EditText) findViewById(R.id.paymentMethod);
+        TextView paymentinfo = (TextView) findViewById(R.id.paymentInfo);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            Boolean condition = extras.getBoolean("boolean");
+if (condition == true){
+    paymentMethod.setVisibility(View.INVISIBLE);
+    paymentinfo.setVisibility(View.INVISIBLE);
+} else {
+    paymentMethod.setVisibility(View.VISIBLE);
+    paymentinfo.setVisibility(View.VISIBLE);
+}
+
+        }
+
+        Button button = (Button) findViewById(R.id.buttonFinish);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (TextUtils.isEmpty(fullName.getText().toString()) || TextUtils.isEmpty(question1.getText().toString()) ||TextUtils.isEmpty(question2.getText().toString())) {
+                    Context context = getApplicationContext();
+                    CharSequence text = "Please enter value.";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                } else {
+                    openThankYouActivity();
+                }
+
+            }
+        });
+    }
+
+    public void openThankYouActivity() {
+
+        Intent intent = new Intent(this, ThankYouActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/demariodouce/demariodouce_comp304sec003_lab2_ex1/PaymentActivity.java
+++ app/src/main/java/com/demariodouce/demariodouce_comp304sec003_lab2_ex1/PaymentActivity.java
@@ -2,7 +2,15 @@ package com.demariodouce.demariodouce_comp304sec003_lab2_ex1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class PaymentActivity extends AppCompatActivity {
 
@@ -10,5 +18,41 @@ public class PaymentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
+
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        Button button = (Button) findViewById(R.id.buttonPay);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (radioGroup.getCheckedRadioButtonId() == -1)
+                {
+                    Context context = getApplicationContext();
+                    CharSequence text = "Please select an option.";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
+                else
+                {
+                    openConfirmationActivity();
+                }
+
+            }
+        });
     }
+    public void openConfirmationActivity() {
+        RadioButton radioButton3 = (RadioButton) findViewById(R.id.radioButton3);
+        if(radioButton3.isChecked()){
+            Intent intent = new Intent(this, ConfirmationActivity.class);
+            intent.putExtra("boolean",true);
+            startActivity(intent);
+        } else {
+            Intent intent = new Intent(this, ConfirmationActivity.class);
+            intent.putExtra("boolean",false);
+            startActivity(intent);
+        }
+
+    }
+
 }
